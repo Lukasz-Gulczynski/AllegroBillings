276f7545b0372d884029df6a3c5cdf28531f4c3e
Lukasz Gulczynski <lukasz.gulczynski90@gmail.com>
2024-06-28 12:52:26 +0200

    changed from tested order for orders from db

--- AllegroBillings.BusinessLogic/Commands/OrderBillingServiceCommand.cs
+++ AllegroBillings.BusinessLogic/Commands/OrderBillingServiceCommand.cs
@@ -19,7 +19,7 @@ namespace AllegroBillingEntries.BussinesLogic.Commands
                 var filters = new Dictionary<string, string>
                 {
                     { "marketplaceId", "allegro-pl" },
-                    { "order.id", "39dfb9b0-1667-1667-11ee-a9ed-5cb2e49c7660" },
+                    { "order.id", orderId },
                     { "limit", "100" },
                     { "offset", "0" }
                 };
